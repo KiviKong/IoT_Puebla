ed7675575243f36624b02ad8bd80ea25082d48a7
KiviKong <kevin95ra@gmail.com>
2018-03-25 23:52:58 -0600

    Update successfuly a visitor

--- src/js/index.js
+++ src/js/index.js
@@ -5,27 +5,57 @@ $('#alerta').click( () => {
 });
 
 $('#aws').click( () => {
-    let data = {
-        "x":"2", 
-        "y":"2", 
-        "deviceID":"45"
-    }
-    $.ajax({
-        url: 'https://ij8qqglkn0.execute-api.us-east-2.amazonaws.com/Develop',
-        type: 'POST',
-        crossDomain: true,
-        contentType: 'application/json',
-        data: JSON.stringify({"x":"2", "y":"2", "deviceID":"45"}),
-        dataType: 'json',
-        success: (data) => {
+
+    swal({
+        title: "Confirmar Registro",
+        text: "Verifica que los datos sean correctos",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true,
+      })
+    .then((willSend) => {
+
+        if (willSend) {
+
+            let data = {
+                "x": document.getElementById("x").value, 
+                "y": document.getElementById("y").value, 
+                "deviceID": document.getElementById("device").value,
+                "name": document.getElementById("name").value,
+                "age": document.getElementById("age").value,
+                "curp": document.getElementById("curp").value,
+                "tel": document.getElementById("tel").value,
+                "turist": $("#turist").prop('checked'), 
+            }
+
             console.log(data);
-            swal('Wea Actualizada');
-        },
-        error: function(xhr, ajaxOptions, thrownError) {
-            console.log(thrownError);
-            swal('Wea NO Actualizada');
+        
+            $.ajax({
+                url: 'https://ij8qqglkn0.execute-api.us-east-2.amazonaws.com/Develop/register',
+                type: 'POST',
+                crossDomain: true,
+                contentType: 'application/json',
+                data: JSON.stringify(data),
+                dataType: 'json',
+                success: (data) => {
+                    if (data === 'ok')
+                        swal("Visitante Registrado", {icon: "success",});
+                    else
+                        swal("Error en el registro", {icon: "error",});
+
+                },
+                error: function(xhr, ajaxOptions, thrownError) {
+                    console.log(thrownError);
+                    swal("Error en el registro", {icon: "error",});
+                }
+            });
+
+        } else {
+            swal("Registro cancelado", {icon: "error",});
         }
     });
+
+    
 });
 
 
