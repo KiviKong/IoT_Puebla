b0c63bd8bab5004477f34b837c5443fc9bc7859b
KiviKong <kevin95ra@gmail.com>
2018-03-25 18:51:18 -0600

    Conects to aws Api gateway

--- src/js/index.js
+++ src/js/index.js
@@ -4,4 +4,23 @@ $('#alerta').click( () => {
     swal('Hello world');
 });
 
+$('#aws').click( () => {
+    $.ajax({
+        url: 'https://ij8qqglkn0.execute-api.us-east-2.amazonaws.com/Develop',
+        type: 'POST',
+        crossDomain: true,
+        contentType: 'application/json',
+        data: JSON.stringify({"x":"2", "y":"2", "deviceID":"45"}),
+        dataType: 'json',
+        success: (data) => {
+            console.log(data);
+            swal('Wea Actualizada');
+        },
+        error: function(xhr, ajaxOptions, thrownError) {
+            console.log(thrownError);
+            swal('Wea NO Actualizada');
+        }
+    });
+});
+
 
