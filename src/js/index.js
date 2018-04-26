b61431f6403f655b4b155b18154289df0497ae50
GiltosMx <adriantoscanop@gmail.com>
2018-04-25 21:07:59 -0500

    Agrega registro para salida de visitantes

--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,65 @@ $('#alerta').click( () => {
     swal('Hello world');
 });
 
+
+$('#btnRegistrar').click( () => {    
+    
+    document.getElementById('actionChooser').style="visibility: hidden";
+    document.getElementById('registerForm').style="visibility: visible";
+});
+
+$('#btnFinalizar').click( () => {
+    document.getElementById('actionChooser').style="visibility: hidden";
+    document.getElementById('finalizeForm').style="visibility: visible";
+});
+
+$('#btnTerminarVisita').click( () => {
+
+    swal({
+        title: "Confirmar Registro",
+        text: "Verifica que los datos sean correctos",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true,
+      })
+    .then((willSend) => {
+
+        if (willSend) {
+
+            let data = {
+                "deviceid": document.getElementById('deviceID').value
+            };
+        
+            $.ajax({
+                url: 'https://6tsqyoqtvg.execute-api.us-east-2.amazonaws.com/develop/exitvisitor' ,
+                type: 'POST',
+                crossDomain: true,
+                contentType: 'application/json',
+                data: JSON.stringify(data),
+                dataType: 'json',
+                success: (data) => {
+                    if (data) {
+                        console.log(data);
+                        swal("Salida exitosa", {icon: "success",});
+                        document.getElementById('finalizeForm').style="visibility: hidden";
+                        document.getElementById('actionChooser').style="visibility: visible";  
+                    }
+                    else
+                        swal("Error en la salida", {icon: "error",});
+
+                },
+                error: function(xhr, ajaxOptions, thrownError) {
+                    console.log(thrownError);
+                    swal("Error en la salida", {icon: "error",});
+                }
+            });
+
+        } else {
+            swal("Finalización cancelada", {icon: "error",});
+        }
+    });  
+});
+
 $('#aws').click( () => {
 
     swal({
@@ -38,8 +97,11 @@ $('#aws').click( () => {
                 data: JSON.stringify(data),
                 dataType: 'json',
                 success: (data) => {
-                    if (data === 'ok')
+                    if (data === 'ok') {
                         swal("Visitante Registrado", {icon: "success",});
+                        document.getElementById('registerForm').style="visibility: hidden";
+                        document.getElementById('actionChooser').style="visibility: visible";
+                    }
                     else
                         swal("Error en el registro", {icon: "error",});
 
